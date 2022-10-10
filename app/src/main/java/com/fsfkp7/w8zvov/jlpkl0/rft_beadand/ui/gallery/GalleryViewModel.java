0097cbea92b9e2f9ebef36068e41efbbc0d9b0e1
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2022-10-10 19:57:25 +0200

    Initial commit

---
+++ app/src/main/java/com/fsfkp7/w8zvov/jlpkl0/rft_beadand/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.fsfkp7.w8zvov.jlpkl0.rft_beadand.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
