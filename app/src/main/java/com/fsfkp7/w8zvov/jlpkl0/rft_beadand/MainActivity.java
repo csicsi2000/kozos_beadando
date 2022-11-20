cca810e38aa93972998136a663236be4802427d6
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2022-11-20 20:09:56 +0100

    make build clean

--- app/src/main/java/com/fsfkp7/w8zvov/jlpkl0/rft_beadand/MainActivity.java
+++ app/src/main/java/com/fsfkp7/w8zvov/jlpkl0/rft_beadand/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
