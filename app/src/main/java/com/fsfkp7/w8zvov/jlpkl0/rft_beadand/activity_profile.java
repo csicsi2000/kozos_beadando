0d8edd314ddcc7196ca74ae06adb99d3d1cf7a2f
Sandor Kis <kisandor@hotmail.com>
2022-11-26 14:38:45 +0000

    profile picture update implemented

--- app/src/main/java/com/fsfkp7/w8zvov/jlpkl0/rft_beadand/activity_profile.java
+++ app/src/main/java/com/fsfkp7/w8zvov/jlpkl0/rft_beadand/activity_profile.java
@@ -1,9 +1,20 @@
 package com.fsfkp7.w8zvov.jlpkl0.rft_beadand;
 
+import static androidx.core.content.ContentProviderCompat.requireContext;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -21,6 +32,7 @@ import com.fsfkp7.w8zvov.jlpkl0.rft_beadand.data.SubjectAdapter;
 import com.fsfkp7.w8zvov.jlpkl0.rft_beadand.data.Teacher;
 import com.teacher.sqlitedatabase.SQLiteDatabaseHandler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,6 +41,8 @@ public class activity_profile extends AppCompatActivity {
     ArrayList<ISubject> subjectList;
     Button btn_update;
     ListView subject_list;
+    ImageView avatar;
+    ActivityResultLauncher<Intent> activityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,6 +86,7 @@ public class activity_profile extends AppCompatActivity {
         btn_update = findViewById(R.id.button_update);
 
         btn_update.setOnClickListener(new View.OnClickListener() {
+            //updating profile
             @Override
             public void onClick(View view) {
                 View v;
@@ -94,23 +109,47 @@ public class activity_profile extends AppCompatActivity {
                 for (int j = 0; j< updated_subject.size(); j++){
                     teacher.subjects.add(updated_subject.get(j));
                 }
-
                 addTeacher(teacher);
-
-                /*
-                ITeacher updated_teacher = new Teacher(
-                        teacher.name,
-                        teacher.image,
-                        updated_subject,
-                        email.getText().toString(),
-                        phone.getText().toString(),
-                        teacher.password
-                );
-                addTeacher(updated_teacher);
-                */
             }
         });
+
+        //setting up profile picture
+        avatar = findViewById(R.id.ImageView_Profile);
+            avatar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    activityResultLauncher.launch(intent);
+                }
+            });
+        ContentResolver contentResolver = getContentResolver();
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == RESULT_OK){
+                            Intent intent = result.getData();
+                            Uri selectedImage = intent.getData();
+                            Bitmap newAvatar = null;
+                            try {
+                                newAvatar = MediaStore.Images.Media.getBitmap(
+                                        contentResolver, selectedImage);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            avatar.setImageURI(selectedImage);
+                            teacher.image = newAvatar;
+                        }
+                    }
+
+
+                });
+
+
     }
+
+
     public void addTeacher(ITeacher tc){
         IDatabaseHandler dbHandler =  new SQLiteDatabaseHandler(getApplicationContext());
         Boolean value = dbHandler.addOrEditTeacher((tc));
